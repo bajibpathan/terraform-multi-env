58e74a5416abe16663cd504de6cf9c7c94002c6d
Baji Pathan <bajibpathan@gmail.com>
2025-06-15 21:00:14 -0400

    Initial Commit

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,6 @@
+    bucket = "baji-devops-tf-remotestate-dev"
+    key    = "tfvars-demo"
+    region = "us-east-1"
+    # dynamodb_table = "baji-devops-tf-remotestate"
+    encrypt      = true
+    use_lockfile = true
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,6 @@
+    bucket = "baji-devops-tf-remotestate-prod"
+    key    = "tfvars-demo"
+    region = "us-east-1"
+    # dynamodb_table = "baji-devops-tf-remotestate"
+    encrypt      = true
+    use_lockfile = true
